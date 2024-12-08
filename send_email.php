80d30a0c5f8dee3ed66fec8d2c81b7e916f7b47b
Atulkrishnan MU <115357632+AtulkrishnanMU@users.noreply.github.com>
2024-12-08 19:20:50 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,25 +1,10 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validate and sanitize user input
-    if (isset($_POST['user_message']) && !empty($_POST['user_message'])) {
-        $message = strip_tags(trim($_POST['user_message'])); // Sanitize input (remove HTML tags)
+// the message
+$msg = "First line of text\nSecond line of text";
 
-        $to = "travisbickleis15482@gmail.com"; // The recipient's email address
-        $subject = "Message from HTML Form"; // Subject of the email
-        $headers = "From: travisbickleis15482@gmail.com\r\n"; // The "from" email address
-        $headers .= "Reply-To: travisbickleis15482@gmail.com\r\n"; // Optional, reply-to header
-        
-        // Additional headers
-        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n"; // Set content type to plain text
-        
-        // Send email
-        if (mail($to, $subject, $message, $headers)) {
-            echo "Email sent successfully!";
-        } else {
-            echo "There was an error sending the email.";
-        }
-    } else {
-        echo "Message field is empty or not set.";
-    }
-}
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("travisbickleis15482@gmail.com","My subject",$msg);
 ?>
